ded733734c4f763679967ff4bc19399d09090ce3
qus <sining.qu@amadeus.com>
2016-07-14 19:29:35 +0200

    ui changed

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,73 @@
+angular.module('app.controllers', [])
+  
+.controller('cAPUPACtrl', function($scope) {
+
+})
+   
+.controller('discoverChallengeCtrl', function($scope) {
+
+})
+   
+.controller('discoverChallenge2Ctrl', function($scope) {
+
+})
+   
+.controller('myChallengeCtrl', function($scope) {
+
+})
+   
+.controller('meCtrl', function($scope) {
+
+})
+   
+.controller('me2Ctrl', function($scope) {
+
+})
+   
+.controller('myChallenge2Ctrl', function($scope) {
+
+})
+   
+.controller('myChallenge3Ctrl', function($scope) {
+
+})
+   
+.controller('myChallenge4Ctrl', function($scope) {
+
+})
+   
+.controller('myChallengeProposerCtrl', function($scope) {
+
+})
+   
+.controller('myChallengeProposer2Ctrl', function($scope) {
+
+})
+   
+.controller('myChallengeProposer3Ctrl', function($scope) {
+
+})
+   
+.controller('myChallengeProposer4Ctrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+	var ref = new Firebase("https//papsy-d9aa0.firebaseio.com");
+	  $scope.addUser = function(){
+	  	console.log(this.signup.username);
+	  	console.log(this.signup.password);
+
+	  	firebase.auth().createUserWithEmailAndPassword("bobtony@firebase.com","correcthorsebatterystaple").catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  // ...
+		});
+	  };
+})
+ 
